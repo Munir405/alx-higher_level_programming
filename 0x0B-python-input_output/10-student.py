7c0f97fec5efaf201410fdee5e52ca9d3004903d
akubosylva <akubosilver@gmail.com>
2023-01-11 00:06:47 +0100

    Input/Output

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -5,14 +5,11 @@
 class Student:
     """Student class"""
     def __init__(self, first_name, last_name, age):
-        """Initializes a new Student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """Return a dictionary representaion of the object based on the keys
-        contained in attrs"""
         if attrs is None:
             return self.__dict__
         new_dict = {}
@@ -20,9 +17,3 @@ class Student:
             if key in self.__dict__:
                 new_dict[key] = self.__dict__[key]
         return new_dict
-
-    def reload_from_json(self, json):
-        """Replace all attributes of a Student instance
-        that are present in json"""
-        for key, value in json.items():
-            self.__dict__[key] = value
