dba4fa5dec5afda87f697a993ef59aea799c781f
akubosylva <akubosilver@gmail.com>
2023-01-11 00:47:20 +0100

    Input/Output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,30 +1,19 @@
 #!/usr/bin/python3
-"""
-Module 7-add_item
-
-Contains funtion that adds and save to python obj to JSON file; loads objects
-
-# run with ./7-add_item.py
-#
-# cat add_item.json ; echo ""
-# expect output: []
-#
-# ./7-add_item.py some random args
-# cat add_item.json ; echo ""
-# expect output: ["some", "random", "args"]
-
-"""
+"""list add module"""
+import sys
+import json
 
 
-from sys import argv
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6_load_from_json_file').load_from_json_file
-
-filename = "add_item.json"
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
 try:
-    existing_content = load_from_json_file(filename)
-except FileNotFoundError:
-    existing_content = []
+    my_list = load_from_json_file("add_item.json")
+except Exception:
+    my_list = []
+    save_to_json_file(my_list, "add_item.json")
 
-save_to_json_file(existing_content + argv[1:], filename)
+for i in range(1, len(sys.argv)):
+    my_list.append(sys.argv[i])
+
+save_to_json_file(my_list, "add_item.json")
