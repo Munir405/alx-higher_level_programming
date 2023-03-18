7c46cb2950e5fae5362e7265447d77a5a27097ac
akubosylva <akubosilver@gmail.com>
2023-03-18 12:47:45 +0100

    Modify file

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,14 +1,6 @@
 #!/usr/bin/node
-
 exports.esrever = function (list) {
-  let beg = 0;
-  let end = list.length - 1;
-  while (beg < end) {
-    let tmp = list[beg];
-    list[beg] = list[end];
-    list[end] = tmp;
-    beg++;
-    end--;
-  }
-  return list;
+  const arr = [];
+  list.forEach((element) => arr.unshift(element));
+  return arr;
 };
