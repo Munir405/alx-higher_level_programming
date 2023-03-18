c4bf25f57656619cb51c9d01e593340a9b9522f5
akubosylva <akubosilver@gmail.com>
2023-03-18 14:10:04 +0100

    Modify file

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -4,18 +4,14 @@ Defines class City
 """
 
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from relationship_state import Base, State
-
-# Base = declarative_base()
+from model_state import Base
 
 
 class City(Base):
     """
-    Class City; instance of Base
-    Linked to MySQL table "city"
+    A class representation of the City table of mysql database
     """
     __tablename__ = "cities"
-    id = Column(Integer, nullable=False, primary_key=True)  # autoincrements
-    name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey(State.id), nullable=False)
+    id = Column("id", Integer, primary_key=True)
+    name = Column("name", String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -4,19 +4,18 @@ Defines class State
 """
 
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-# from relationship_city import City
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
 
 class State(Base):
     """
-    Class State; instance of Base
-    Linked to MySQL table "states"
+    A class representation of the State table of mysql database
     """
     __tablename__ = "states"
-    id = Column(Integer, nullable=False, primary_key=True)
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state")
+    id = Column("id", Integer, primary_key=True)
+    name = Column("name", String(128), nullable=False)
+
+    cities = relationship('City', backref='state')
