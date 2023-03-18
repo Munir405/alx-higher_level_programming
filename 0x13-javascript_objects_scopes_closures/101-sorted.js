b42964b90131f8074a8a9096abf3c7df9186b699
akubosylva <akubosilver@gmail.com>
2023-03-18 12:52:53 +0100

    Modify file

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,13 +1,11 @@
 #!/usr/bin/node
-
 const dict = require('./101-data').dict;
-
-let newDic = {};
-for (let key in dict) {
-  if (newDic[dict[key]] === undefined) {
-    newDic[dict[key]] = [];
+const map = {};
+for (const key in dict) {
+  if (map[dict[key]]) {
+    map[dict[key]].push(key);
+  } else {
+    map[dict[key]] = [key];
   }
-  newDic[dict[key]].push(key);
 }
-
-console.log(newDic);
+console.log(map);
