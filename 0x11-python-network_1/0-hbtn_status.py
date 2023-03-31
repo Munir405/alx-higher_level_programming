18e232e843823b14112f6328a69528e1405d4fc3
akubosylva <akubosilver@gmail.com>
2023-03-31 13:02:01 +0100

    Modifty file

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """
-fetch https://alx-intranet.hbtn.io/status; display response
+fetch https://ialx-intranet.hbtn.io/status; display response
 """
 
 import urllib.request
 
 if __name__ == "__main__":
-    response = urllib.request.urlopen("https://alx-intranet.hbtn.io/status").read()
-    print("Body response:")
-    print(f"\t- type: {type(response)}")
-    print(f"\t- content: {response}")
-    print(f"\t- utf8 content: {response.decode('utf-8')}")
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        html = response.read()
+        print('Body response:')
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode('utf-8')))
