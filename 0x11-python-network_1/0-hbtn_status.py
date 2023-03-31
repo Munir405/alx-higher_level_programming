fe738ccfa86503e0ea0463074743edfde8804d7f
akubosylva <akubosilver@gmail.com>
2023-03-31 12:58:24 +0100

    Modify file

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,8 +4,10 @@ fetch https://alx-intranet.hbtn.io/status; display response
 """
 
 import urllib.request
-response = urllib.request.urlopen("https://alx-intranet.hbtn.io/status").read()
-print("Body response:")
-print(f"\t- type: {type(response)}")
-print(f"\t- content: {response}")
-print(f"\t- utf8 content: {response.decode('utf-8')}")
+
+if __name__ == "__main__":
+    response = urllib.request.urlopen("https://alx-intranet.hbtn.io/status").read()
+    print("Body response:")
+    print(f"\t- type: {type(response)}")
+    print(f"\t- content: {response}")
+    print(f"\t- utf8 content: {response.decode('utf-8')}")
